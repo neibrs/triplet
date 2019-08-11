74af82202317eab1c21894255846bad4e6c88f5e
John <lijd@eabax.com>
2019-08-11 20:49:54 +0800

    fix.

--- bin/install.sh
+++ bin/install.sh
@@ -7,13 +7,13 @@ rm web/sites/default/settings.php
 #vendor/bin/drush site:install -y --account-pass=admin --db-url=mysql://root:root@mariadb/laradrupal --site-name="NIDC"
 vendor/bin/drush site:install -y --account-pass=admin --db-url=mysql://root:root@127.0.0.1:8889/triplet --site-name="NIDC"
 
-#echo "ini_set('memory_limit', -1);" > web/sites/default/settings.php
-
 chmod -R a+rw web/sites/default
+echo "ini_set('memory_limit', -1);" >> web/sites/default/settings.php
 vendor/bin/drupal site:mode dev
 
 vendor/bin/drush en -y adminimal_admin_toolbar \
   coffee \
+  commerce_demo \
   config_translation \
   config_update_ui \
   memcache \
@@ -41,6 +41,6 @@ vendor/bin/drush en -y adminimal_admin_toolbar \
 #vendor/bin/drush cset -y system.site default_langcode "zh-hans"
 #vendor/bin/drush locale:update
 
-#bin/drupal thi -y neibers_codex
-#vendor/bin/drush cset -y system.theme admin neibers_codex
-#vendor/bin/drush cset -y system.theme default neibers_codex
+vendor/bin/drupal thi -y smarts
+vendor/bin/drush cset -y system.theme admin smarts
+vendor/bin/drush cset -y system.theme default smarts
